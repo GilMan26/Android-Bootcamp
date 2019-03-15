51c3c683481273ed0766722dfaf12eb2d8d04363
Mandeep <mandeep.gill@tothenew.com>
2019-03-15 14:17:44 +0530

    Changes

--- Assignment5/app/src/main/java/com/example/assignment5/Ques3/ActivityQues3.java
+++ Assignment5/app/src/main/java/com/example/assignment5/Ques3/ActivityQues3.java
@@ -1,7 +1,8 @@
 package com.example.assignment5.Ques3;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,24 +10,26 @@ import android.view.View;
 import com.example.assignment5.R;
 
 public class ActivityQues3 extends AppCompatActivity {
-    FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ques3);
-        fragmentManager=getSupportFragmentManager();
+
 
     }
 
     public void showDialog(View view){
+        FragmentManager fragmentManager=getSupportFragmentManager();
         DialogFragmentExample diag=new DialogFragmentExample();
         diag.show(fragmentManager,"Dialog Fragment");
     }
 
     public void showPreference(View view){
+        android.app.FragmentManager fragmentManager = getFragmentManager();
         PrefFragment prefFragment=new PrefFragment();
-        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
-//        fragmentTransaction.add(R.id.frameLayout, prefFragment, "Preference Fragment");
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.frameLayout, prefFragment, "Preference Fragment");
         fragmentTransaction.commit();
     }
 }
